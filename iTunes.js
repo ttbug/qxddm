5b3034b542f4c9843252334facb5e91c2e636003
Gertrude Martin <ttbuge@gmail.com>
2025-04-08 16:50:40 +0800

    Update iTunes.js

--- iTunes.js
+++ iTunes.js
@@ -12,7 +12,7 @@
 **************************************
 
 [rewrite_local]
-^https?:\/\/buy\.itunes\.apple\.com\/verifyReceipt$ url script-response-body https://raw.githubusercontent.com/chxm1023/Rewrite/main/iTunes.js
+^https?:\/\/buy\.itunes\.apple\.com\/verifyReceipt$ url script-response-body https://raw.githubusercontent.com/ttbug/qxddm/main/iTunes.js
 
 [mitm]
 hostname = buy.itunes.apple.com
