7cb599f470b33d55353aa8ec9f920ef0c058f142
Gertrude Martin <ttbuge@gmail.com>
2025-04-08 16:53:11 +0800

    Update Reheji.js

--- Reheji.js
+++ Reheji.js
@@ -9,8 +9,8 @@
 **************************************
 
 [rewrite_local]
-^https:\/\/api\.(revenuecat|rc-backup)\.com\/.+\/(receipts$|subscribers\/?(.*?)*$) url script-response-body https://raw.githubusercontent.com/chxm1023/Rewrite/main/Reheji.js
-^https:\/\/api\.(revenuecat|rc-backup)\.com\/.+\/(receipts$|subscribers\/?(.*?)*$) url script-request-header https://raw.githubusercontent.com/chxm1023/Rewrite/main/Reheji.js
+^https:\/\/api\.(revenuecat|rc-backup)\.com\/.+\/(receipts$|subscribers\/?(.*?)*$) url script-response-body https://raw.githubusercontent.com/ttbug/qxddm/main/Reheji.js
+^https:\/\/api\.(revenuecat|rc-backup)\.com\/.+\/(receipts$|subscribers\/?(.*?)*$) url script-request-header https://raw.githubusercontent.com/ttbug/qxddm/main/Reheji.js
 
 [mitm]
 hostname = api.revenuecat.com, api.rc-backup.com
